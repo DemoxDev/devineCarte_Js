8f8775e64c7eb913e6e4c2af7be8386cef83a1af
Adrien S <adrien.savoie@enc-bessieres.org>
2022-11-01 19:18:06 +0100

    Correction jeu 52 cartes

--- source/PartieAJouer.js
+++ source/PartieAJouer.js
@@ -21,7 +21,7 @@ if (nb_cartes == 32) {
 	var paquetDeCartes = new Paquet(Paquet.createJeu32Cartes());
 } else {
 	console.log("Création d'un paquet de 52 cartes");
-	var paquetDeCartes = new Paquet(Paquet.createJeu32Cartes());
+	var paquetDeCartes = new Paquet(Paquet.createJeu52Cartes());
 }
 let aide = "";
 // Donner au joueur le choix de reçevoir de l'aide à chaque essai
