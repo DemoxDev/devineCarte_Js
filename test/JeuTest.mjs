cf34248a772b4802a220cfa342ebb9182eb50c7a
Adrien S <adrien.savoie@enc-bessieres.org>
2022-11-08 11:22:07 +0100

    Initialisation test unitaire Jeu

---
+++ test/JeuTest.mjs
@@ -0,0 +1,15 @@
+import { Carte } from "../source/Carte.js";
+import assert from "assert";
+import { NomCarte } from "../source/NomCarte.js";
+import { Couleur } from "../source/Couleur.js";
+import { Paquet } from "../source/Paquet.js";
+import { Jeu } from "../source/Jeu.js";
+
+describe("Tests relatifs au jeu", function () {
+	describe("Tests relatifs à l'aide", function () {
+		it('L\'indice est "plus grand" si la carte proposée est trop petite', function () {
+			//
+			// assert.strictEqual();
+		});
+	});
+});
